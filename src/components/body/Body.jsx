c2ec0ecebc4bd0385873a2486f051ff9d54924a9
Camilo <137653825+CXmiloxx@users.noreply.github.com>
2024-06-01 11:53:11 -0500

    añadiendo la informacion de las palabras cambiando estilos de la pagina y añadiendo nueva funcionnalidad -d

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Body.css';
-import { palabrasIngles, palabrasEspanol, pronunciaciones } from './palabras';
-import descripcionesPalabras from './descripcionesPalabras'; 
+import { palabrasIngles, palabrasEspanol, pronunciaciones } from './palabras'; // Importar datos de palabras en inglés, español y pronunciaciones
+import descripcionesPalabras from './descripcionesPalabras'; // Importar descripciones de palabras
 
 function Body() {
-    const [resultados, setResultados] = useState([]);
-    const [letraSeleccionada, setLetraSeleccionada] = useState('');
+    const [resultados, setResultados] = useState([]); // Estado para almacenar los resultados de la búsqueda
+    const [letraSeleccionada, setLetraSeleccionada] = useState(''); // Estado para almacenar la letra seleccionada
 
+    // Función para buscar términos que comiencen con la letra proporcionada
     const buscarTerminos = (letra) => {
         const resultadosLetra = [];
 
@@ -25,10 +26,16 @@ function Body() {
             }
         }
 
-        setResultados(resultadosLetra);
-        setLetraSeleccionada(letra);
+        setResultados(resultadosLetra); // Actualizar resultados de la búsqueda
+        setLetraSeleccionada(letra); // Actualizar letra seleccionada
     }
 
+    useEffect(() => {
+        // Cuando el componente se monta por primera vez, buscar palabras que comiencen con "A"
+        buscarTerminos('A');
+    }, []); // El segundo argumento del useEffect (un array vacío) asegura que este efecto se ejecute solo una vez
+
+    // Función para crear botones de letras
     const crearBotonesLetras = () => {
         const letras = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
         return letras.map(letra => (
@@ -38,9 +45,10 @@ function Body() {
         ));
     }
 
-    const totalPalabras = palabrasIngles.length;
+    const totalPalabras = palabrasIngles.length; // Total de palabras en el glosario
 
-    const flipCard = (index) => {
+    // Función para girar la tarjeta al hacer hover
+    const volterarCard = (index) => {
         const cards = document.querySelectorAll('.card');
         cards.forEach((card, i) => {
             if (i === index) {
@@ -68,39 +76,52 @@ function Body() {
         speechSynthesis.speak(mensaje);
     }
 
+    // Función para volver al inicio de la página
+    const VolverIncio = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Desplazamiento suave
+        });
+    };
+
     return (
         <div className="body-container">
-            <p className="total-palabras">Total words in the glossary: {totalPalabras}</p>
+            <p className="total-palabras">Total palabras en el glosario: {totalPalabras}</p>
             <div className="abecedario-container">
                 {crearBotonesLetras()}
             </div>
-            <p>WORDS WITH THE LETTER :{letraSeleccionada}</p>
+            <p className='palabras'>PALABRAS CON LA LETRA: {letraSeleccionada}</p>
+            <div className='btnVolver'>
+                <button className='btnParaVolver' onClick={VolverIncio}>volver</button>
+            </div>
             <div className="resultado-container">
                 {resultados.length > 0 ? (
                     resultados.map((resultado, index) => (
-                        <div key={index} className="card" onMouseEnter={() => flipCard(index)} onMouseLeave={() => flipCard(index)}>
+                        <div key={index} className="card" onMouseEnter={() => volterarCard(index)} onMouseLeave={() => volterarCard(index)}>
                             <div className="card-inner">
                                 <div className="card-front">
-                                    <p className='descripcion'>{resultado.descripcion}</p>
-                                    <p>Word number:{resultado.numeroPalabra}</p>
+                                    <h2 className="titulo">{resultado.palabraIngles}</h2>
+                                    <p className='palabras'>{resultado.descripcion}</p>
+                                    <p className='palabras'>Número de palabra: {resultado.numeroPalabra}</p>
                                     {/* Botón para pronunciar la palabra */}
-                                    <button className="pronunciar-btn" onClick={() => pronunciarPalabra(resultado.palabraIngles)}>to pronounce</button>
-
+                                    <button className="pronunciar-btn" onClick={() => pronunciarPalabra(resultado.palabraIngles)}>pronunciar</button>
                                 </div>
                                 <div className="card-back">
-                                    <h5 className='palabraEspanol'>Palabra en español: {resultado.palabraEspanol}</h5>
-                                    <h5 className='palabraIngles'>Palabra en inglés: {resultado.palabraIngles}</h5>
-                                    <span className="pronunciacion">Fonética: {resultado.pronunciacion}</span>
+                                    <h2 className="titulo">INFORMACIÓN</h2>
+                                    <p className='palabras-atras'>Palabra en inglés: {resultado.palabraIngles}</p>
+                                    <p className='palabras-atras'>Palabra en español: {resultado.palabraEspanol}</p>
+                                    <p className="palabras-atras">Fonética: {resultado.pronunciacion}</p>
                                     {/* Botón para pronunciar la palabra */}
-                                    <button className="pronunciar-btn" onClick={() => pronunciarPalabra(resultado.palabraIngles)}>to pronounce</button>
+                                    <button className="pronunciar-btn" onClick={() => pronunciarPalabra(resultado.palabraIngles)}>pronunciar</button>
                                 </div>
                             </div>
                         </div>
                     ))
                 ) : (
-                    <div>No terms were found for the selected letter.</div>
+                    <div>No se encontraron términos para la letra seleccionada.</div>
                 )}
             </div>
+            
         </div>
     );
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -9,22 +9,6 @@ function Nav() {
                 <img src={fondo} alt="Logo" className="logo-img" />
             </div>
             <h1>Glosary Spanish And English</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="/" className="nav-link">Inicion</a>
-                    </li>
-                    <li>
-                        <a href="/about" className="nav-link">Sobre Nosotros</a>
-                    </li>
-                    <li>
-                        <a href="/contact" className="nav-link">Contacto</a>
-                    </li>
-                    <li>
-                        <a href="https://www.youtube.com/watch?v=XXYlFuWEuKI" className="nav-link">Video</a>
-                    </li>
-                </ul>
-            </nav>
         </header>
     );
 }
